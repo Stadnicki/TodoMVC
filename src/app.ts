e6bd8106fb6a20d4484aa8bef4e1c8f8ae7aa2ee
Adrian Stadnicki <astadnicki@sii.pl>
2024-11-19 13:12:06 +0100

    Toggle all

--- src/app.ts
+++ src/app.ts
@@ -1,172 +1,226 @@
 import {LitElement, css, html} from 'lit';
 import {customElement, property, state} from 'lit/decorators.js';
-import { Filter } from "./filter.enum.ts";
+import {Filter} from "./filter.enum.ts";
 import {classMap} from "lit/directives/class-map.js";
 
 @customElement('todo-app')
 export class TodoApp extends LitElement {
-  @state()
-  todoList: any[] = [];
+    @state()
+    todoList: any[] = [];
 
-  @property()
-  filterState: Filter = Filter.All;
+    @property()
+    filterState: Filter = Filter.All;
 
-  updateFilteredItems () {
-      const filter = window.location.hash.split('#/')[1]
-      this.filterState = filter as Filter;
+    updateFilteredItems() {
+        const filter = window.location.hash.split('#/')[1]
+        this.filterState = filter as Filter;
 
-      this.todoList = this.todoList.map((todo) => {
-        const filter = window.location.hash.split('#/')[1];
+        this.todoList = this.todoList.map((todo) => {
+            const filter = window.location.hash.split('#/')[1];
 
-        return {...todo, hidden: filter === Filter.Active && todo.checked || filter === Filter.Completed && !todo.checked};
-      });
- }
-
-  render() {
-    this.updateFilteredItems();
-    return html`
-      <div class="c-layout">
-        <h1 class="c-header">todos</h1>
-        <todo-input @add=${this.addTodo}></todo-input>
-
-        <div class="c-layout__window">
-          <ul>
-            ${this.todoList.map((todo, index) => html`
-                <todo-item .value="${todo.name}" .index="${index}" @delete="${this.deleteTodo}" @edit="${this.editTodo}" class="${classMap({ hidden: todo.hidden })}"></todo-item>
-            `)}
-          </ul>
-
-          <div class="c-bottom-info">
-            <div>
-              ${this.todoList.filter((todo) => !todo.checked).length} items left
-            </div>
-           
-            <div>
-                ${this.getFilterItem(Filter.All)}
-                ${this.getFilterItem(Filter.Active)}
-                ${this.getFilterItem(Filter.Completed)}
-            </div>
-  
-            <div>
-              <button @click="${this.clearCompleted}">Clear Completed</button>
-            </div>
-          </div>
-        </div>
-        <div>
-    `;
-  }
-
-  clearCompleted() {
-    this.todoList = this.todoList.filter((todo) => !todo.checked);
-  }
-
-  getFilterItem(filter: Filter) {
-      const classes = { selected: this.filterState === filter };
-      return html`<a class="${classMap(classes)}" href="#/${filter}" @click="${() =>  this.setFilter(filter)}">${filter}</a>`;
-  }
-
-  setFilter(filter: Filter) {
-      debugger;
-        this.filterState = filter;
-  }
-  
-  addTodo(e: CustomEvent) {
-    this.todoList = [...this.todoList, e.detail];
-  }
-
-  deleteTodo(e: CustomEvent) {
-    this.todoList = [...this.todoList.filter((_, index) => index !== e.detail.index)];
-  }
-
-  editTodo(e: CustomEvent) {
-    if(e.detail.checked) {
-      this.todoList = this.todoList.map((todo, index) => {
-        if(index === e.detail.index) {
-          return { ...todo, checked: e.detail.checked };
-        }
-        return todo;
-      });
-    }
-
-    if(e.detail.name) {
-        this.todoList = this.todoList.map((todo, index) => {
-            if(index === e.detail.index) {
-                return {...todo, name: e.detail.name}
-            }
-            return todo;
+            return {
+                ...todo,
+                hidden: filter === Filter.Active && todo.checked || filter === Filter.Completed && !todo.checked
+            };
         });
     }
-  }
 
-  static styles = css`
-    :global {
-      :focus-visible,:focus {
-        box-shadow: inset 0 0 2px 2px #cf7d7d !important;
-        outline: 0 !important;
-      }
-    }
-    
-      .hidden {
-          display: none;
-      }
-      
-      a.selected {
-          border: solid 1px #ce4646;
-          border-color: #ce4646;
-      }
-          
-      ul {
-          margin: 0;
-      }
-      
-    :host {
-      width: 100%;
+    render() {
+        this.updateFilteredItems();
+        return html`
+            <div class="c-layout">
+                <h1 class="c-header">todos</h1>
+                <div class="c-mark-container">
+                    <button class="c-mark" @click="${this.toggleAll}">❯</button>
+                </div>
+                <todo-input @add=${this.addTodo}></todo-input>
+
+                <div class="c-layout__window">
+                    <ul>
+                        ${this.todoList.map((todo, index) => html`
+                            <todo-item .value="${todo.name}" .checked="${todo.checked}" .index="${index}" @delete="${this.deleteTodo}"
+                                       @edit="${this.editTodo}" class="${classMap({hidden: todo.hidden})}"></todo-item>
+                        `)}
+                    </ul>
+
+                    <div class="c-bottom-info">
+                        <div>
+                            ${this.todoList.filter((todo) => !todo.checked).length} items left
+                        </div>
+
+                        <div>
+                            ${this.getFilterItem(Filter.All)}
+                            ${this.getFilterItem(Filter.Active)}
+                            ${this.getFilterItem(Filter.Completed)}
+                        </div>
+
+                        <div>
+                            <button class="c-clear" @click="${this.clearCompleted}">Clear Completed</button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+        `;
     }
 
-    :focus-visible,:focus {
-      box-shadow: inset 0 0 2px 2px #cf7d7d !important;
-      outline: 0 !important;
+    clearCompleted() {
+        debugger;
+        this.todoList = this.todoList.filter((todo) => !todo.checked);
     }
 
-    ul {
-      list-style-type: none;
-      padding-inline-start: 0;
+    getFilterItem(filter: Filter) {
+        const classes = {selected: this.filterState === filter};
+        return html`<a class="c-filter${classMap(classes)}" href="#/${filter}" @click="${() => this.setFilter(filter)}">${filter}</a>`;
     }
-    
-    .c-header {
-      font-size: 80px;
-      font-weight: 200;
-      color: #b83f45;
-      margin-top: 0;
-      margin-bottom: 36px;
-      line-height: 60px;
+
+    setFilter(filter: Filter) {
+        debugger;
+        this.filterState = filter;
     }
-    
-    .c-layout {
-      min-width: 230px;
-      max-width: 550px;
-      padding: 2rem;
-      margin: auto;
-      text-align: center;
+
+    toggleAll() {
+        const allChecked = this.todoList.every((todo) => todo.checked);
+        this.todoList = this.todoList.map((todo) => ({...todo, checked: !allChecked}));
     }
-    
-    .c-bottom-info {
-      display: flex;
-      height: 20px;
-      padding: 10px 15px;
-      justify-content: space-between;
-      background: white;
-      
-      box-shadow: 
-          rgba(0, 0, 0, 0.2) 0 1px 1px, white 0 8px 0 -3px,
-          rgba(0, 0, 0, 0.2) 0 9px 1px -3px, white 0 16px 0 -6px,
-          rgba(0, 0, 0, 0.2) 0 17px 2px -6px;
+
+    addTodo(e: CustomEvent) {
+        this.todoList = [...this.todoList, e.detail];
     }
-  `;
+
+    deleteTodo(e: CustomEvent) {
+        this.todoList = [...this.todoList.filter((_, index) => index !== e.detail.index)];
+    }
+
+    editTodo(e: CustomEvent) {
+        if (e.detail.checked) {
+            this.todoList = this.todoList.map((todo, index) => {
+                if (index === e.detail.index) {
+                    return {...todo, checked: e.detail.checked};
+                }
+                return todo;
+            });
+        }
+
+        if (e.detail.name) {
+            this.todoList = this.todoList.map((todo, index) => {
+                if (index === e.detail.index) {
+                    return {...todo, name: e.detail.name}
+                }
+                return todo;
+            });
+        }
+    }
+
+    static styles = css`
+        :global {
+            :focus-visible, :focus {
+                box-shadow: inset 0 0 2px 2px #cf7d7d !important;
+                outline: 0 !important;
+            }
+        }
+
+        .c-mark-container {
+            display: flex;
+            height: 0;
+        }
+        
+        .c-mark {
+            position: absolute;
+            padding: 16px;
+            border: none;
+            background: none;
+            align-content: center;
+            font-size: 22px;
+            color: rgb(148, 148, 148);
+            transform: rotate(90deg);
+            cursor: pointer;
+        }
+        
+        .hidden {
+            display: none;
+        }
+
+        .c-clear {
+            cursor: pointer;
+            text-decoration: none;
+            appearance: none;
+            background: none;
+            border: 0;
+            font-size: 100%;
+
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+
+        .c-filter {
+            border: 1px solid transparent;
+            border-radius: 3px;
+            color: inherit;
+            margin: 3px;
+            padding: 3px 7px;
+            text-decoration: none;
+
+            &:hover {
+                border-color: #db7676;
+            }
+        }
+
+        a.selected {
+            border: solid 1px #ce4646;
+        }
+
+        ul {
+            margin: 0;
+        }
+
+        :host {
+            width: 100%;
+        }
+
+        :focus-visible, :focus {
+            box-shadow: inset 0 0 2px 2px #cf7d7d !important;
+            outline: 0 !important;
+        }
+
+        ul {
+            list-style-type: none;
+            padding-inline-start: 0;
+        }
+
+        .c-header {
+            font-size: 80px;
+            font-weight: 200;
+            color: #b83f45;
+            margin-top: 0;
+            margin-bottom: 36px;
+            line-height: 60px;
+        }
+
+        .c-layout {
+            min-width: 230px;
+            max-width: 550px;
+            padding: 2rem;
+            margin: auto;
+            text-align: center;
+        }
+
+        .c-bottom-info {
+            display: flex;
+            height: 20px;
+            padding: 10px 15px;
+            justify-content: space-between;
+            background: white;
+
+            box-shadow: rgba(0, 0, 0, 0.2) 0 1px 1px, white 0 8px 0 -3px,
+            rgba(0, 0, 0, 0.2) 0 9px 1px -3px, white 0 16px 0 -6px,
+            rgba(0, 0, 0, 0.2) 0 17px 2px -6px;
+        }
+    `;
 }
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'todo-app': TodoApp;
-  }
+    interface HTMLElementTagNameMap {
+        'todo-app': TodoApp;
+    }
 }
--- src/components/item.ts
+++ src/components/item.ts
@@ -1,170 +1,171 @@
-import { LitElement, css, html } from 'lit';
-import { customElement, property, query } from 'lit/decorators.js';
-import { classMap } from "lit/directives/class-map.js";
+import {LitElement, css, html} from 'lit';
+import {customElement, property, query} from 'lit/decorators.js';
+import {classMap} from "lit/directives/class-map.js";
 
 @customElement('todo-item')
 export class TodoItem extends LitElement {
-  @property ()
-  editing = false;
+    @property()
+    editing = false;
 
-  @property()
-  checked = false;
+    @property()
+    checked = false;
 
-  @property()
-  value = 'Hello';
+    @property()
+    value = 'Hello';
 
-  @property()
-  index = 0;
+    @property()
+    index = 0;
 
-  @query('.c-input', true) _input!: HTMLInputElement;
+    @query('.c-input', true) _input!: HTMLInputElement;
 
-  firstUpdated() {
-    this._input.addEventListener('keydown', (e) => {
-      if (e.key === 'Enter') {
-        this.endEdit();
-        this.edit();
-      }
-    });
-  }
+    firstUpdated() {
+        this._input.addEventListener('keydown', (e) => {
+            if (e.key === 'Enter') {
+                this.endEdit();
+                this.edit();
+            }
+        });
+    }
 
-  render() {
-    const classes = { editing: this.editing };
+    render() {
+        const classes = {editing: this.editing };
 
-    return html`
-      <li class="${classMap(classes)}">
-        <input class="c-input" value=${this.value} @keydown="${this.onInputKeydown}" @blur="${this.edit}">
+        return html`
+            <li class="${classMap(classes)}">
+                <input class="c-input" value=${this.value} @keydown="${this.onInputKeydown}" @blur="${this.edit}">
 
-        <div class="c-preview">
-          <input class="c-checkbox" type="checkbox" .checked=${this.checked ?? false} @change=${this.toggle}>
-          <span class="c-value"  @dblclick="${this.startEdit}">${this.value}</span>
-          <button class="c-delete" @click=${this.delete}>
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-      </li>
+                <div class="c-preview">
+                    <input class="c-checkbox" type="checkbox" .checked=${this.checked} @change=${this.toggle}>
+                    <span class="c-value" @dblclick="${this.startEdit}">${this.value}</span>
+                    <button class="c-delete" @click=${this.delete}>
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            </li>
+        `;
+    }
+
+    static styles = css`
+        :host {
+            display: flex;
+            background: white;
+            border-bottom: 1px solid #ededed;
+        }
+
+        li {
+            flex: 1;
+        }
+
+
+        .editing {
+            .c-input {
+                display: block;
+            }
+
+            .c-preview {
+                display: none;
+            }
+        }
+
+        .c-preview {
+            display: flex;
+        }
+
+        .c-value {
+            flex: 1;
+            text-align: left;
+            word-break: break-all;
+            padding: 8px 16px;
+            font-size: 24px;
+            color: #484848FF;
+        }
+
+        .c-input {
+            display: none;
+            flex: 1;
+            background: white;
+            margin-left: 40px;
+            font-size: 24px;
+            padding: 16px;
+            border: none;
+        }
+
+        .c-delete {
+            background: none;
+            color: red;
+            border: none;
+            font: inherit;
+            cursor: pointer;
+            outline: inherit;
+        }
+
+        .c-checkbox {
+            background-image: url(data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23949494%22%20stroke-width%3D%223%22/%3E%3C/svg%3E);
+            background-repeat: no-repeat;
+            background-position: center left;
+
+            width: 40px;
+            height: 40px;
+            appearance: none;
+            outline: none;
+            cursor: pointer;
+
+            &:checked {
+                background-image: url(data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%2359A193%22%20stroke-width%3D%223%22%2F%3E%3Cpath%20fill%3D%22%233EA390%22%20d%3D%22M72%2025L42%2071%2027%2056l-4%204%2020%2020%2034-52z%22%2F%3E%3C%2Fsvg%3E);
+            }
+        }
     `;
-  }
 
-  static styles = css`
-    :host {
-      display: flex;
-      background: white;
-      border-bottom: 1px solid #ededed;
+    private toggle() {
+        this.checked = !this.checked;
+        const options = {
+            detail: {checked: this.checked, index: this.index},
+            bubbles: true,
+            composed: true,
+        }
+        this.dispatchEvent(new CustomEvent('edit', options));
     }
 
-    li {
-      flex: 1;
+    private edit() {
+        const name = this._input.value.trim();
+        if (name) {
+            const options = {
+                detail: {name, index: this.index},
+                bubbles: true,
+                composed: true,
+            };
+            this.dispatchEvent(new CustomEvent('edit', options));
+        }
+        this.endEdit()
     }
 
-
-    .editing {
-      .c-input {
-        display: block;
-      }
-      .c-preview{
-        display: none;
-      }
-    }
-    
-    .c-preview {
-      display: flex;
-    }
-    
-    .c-value {
-      flex: 1;
-      text-align: left;
-      word-break: break-all;
-      padding: 8px 16px;
-      font-size: 24px;
-      color: #484848FF;
-    }
-    
-    .c-input{
-      display: none;
-      flex: 1;
-      background: white;
-      margin-left: 40px;
-      font-size: 24px;
-      padding: 16px;
-      border: none;
+    private onInputKeydown(e: KeyboardEvent) {
+        if (e.key === 'Enter') {
+            this.edit();
+        }
     }
 
-    .c-delete {
-      background: none;
-      color: red;
-      border: none;
-      font: inherit;
-      cursor: pointer;
-      outline: inherit;
+    private startEdit() {
+        this.editing = true;
+        setTimeout(() => this._input.focus(), 500);
     }
 
-    .c-checkbox {
-        background-image: url(data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23949494%22%20stroke-width%3D%223%22/%3E%3C/svg%3E);
-        background-repeat: no-repeat;
-        background-position: center left;
-      
-      width: 40px;
-      height: 40px;
-      appearance: none;
-      outline: none;
-      cursor: pointer;
-      
-      &:checked {
-        background-image: url(data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%2359A193%22%20stroke-width%3D%223%22%2F%3E%3Cpath%20fill%3D%22%233EA390%22%20d%3D%22M72%2025L42%2071%2027%2056l-4%204%2020%2020%2034-52z%22%2F%3E%3C%2Fsvg%3E);
-      }
+    private endEdit() {
+        this.editing = false;
     }
-  `;
 
-  private toggle() {
-    this.checked = !this.checked;
-    const options = {
-      detail: { checked: this.checked, index: this.index},
-      bubbles: true,
-      composed: true,
+    private delete() {
+        const options = {
+            detail: {index: this.index},
+            bubbles: true,
+            composed: true,
+        }
+        this.dispatchEvent(new CustomEvent('delete', options));
     }
-    this.dispatchEvent(new CustomEvent('edit', options));
-  }
-
-  private edit() {
-    const name = this._input.value.trim();
-    if (name) {
-      const options = {
-        detail: { name, index: this.index},
-        bubbles: true,
-        composed: true,
-      };
-      this.dispatchEvent(new CustomEvent('edit', options));
-    }
-    this.endEdit()
-  }
-
-  private onInputKeydown(e: KeyboardEvent) {
-    if (e.key === 'Enter') {
-      this.edit();
-    }
-  }
-
-  private startEdit() {
-    this.editing = true;
-    setTimeout(() => this._input.focus(), 500);
-  }
-
-  private endEdit() {
-    this.editing = false;
-  }
-
-  private delete() {
-    const options = {
-      detail: {index: this.index},
-      bubbles: true,
-      composed: true,
-    }
-    this.dispatchEvent(new CustomEvent('delete', options));
-  }
 }
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'todo-item': TodoItem;
-  }
+    interface HTMLElementTagNameMap {
+        'todo-item': TodoItem;
+    }
 }
